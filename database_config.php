d03d43d99f7c8982442ce53fcdf2020ed5965008
Nathan <schielenathan@gmail.com>
2020-07-21 19:00:18 +0200

    Adding more instructions

--- database_config.php
+++ database_config.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost";
+  $servername = "127.0.0.1";
   $port = 3306;
-  $username = "username";
-  $password = "password";
-  $dbname = "database";
-  $table = "tablename";
+  $username = "root";
+  $password = "mysql";
+  $dbname = "StudyDatabase";
+  $table = "data";
 ?>
